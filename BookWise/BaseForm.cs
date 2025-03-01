f3633d72bf642985777de5c003b9466c6fd87d5d
Chirath <chirathtimodh@gmail.com>
2025-03-01 20:16:51 +0530

    Remove FormClosing EventHandler

---
+++ BookWise/BaseForm.cs
@@ -0,0 +1,17 @@
+﻿namespace BookWise
+{
+    public class BaseForm : Form
+    {
+        public BaseForm()
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+            ForeColor = Color.FromArgb(30, 41, 59);
+            BackColor = Color.GhostWhite;
+            using (var ms = new MemoryStream(Properties.Resources.app))
+            {
+                Icon = new Icon(ms);
+            }
+        }
+
+    }
+}
