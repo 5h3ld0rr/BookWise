daab5766d6371c67c0ae1162b268b4cb397bbe9f
Chirath <chirathtimodh@gmail.com>
2025-03-04 18:58:11 +0530

    Fix namespace

--- BookWise/Controls/HistoryControl.cs
+++ BookWise/Controls/HistoryControl.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace BookWise.Controls
+﻿namespace BookWise
 {
-    public partial class HistoryControl: UserControl
+    public partial class HistoryControl : UserControl
     {
         public HistoryControl()
         {
             InitializeComponent();
+            RefreshData();
+        }
+        public void RefreshData()
+        {
         }
     }
 }
