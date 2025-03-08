5761c621c1cef6b799475b76c73c6506fa85e655
Chirath <chirathtimodh@gmail.com>
2025-03-08 14:18:01 +0530

    Add onboarding mail

---
+++ BookWise/Classes/Mail.cs
@@ -0,0 +1,35 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+
+namespace BookWise
+{
+    public class Mail
+    {
+        private static readonly string smtpHost = ConfigurationManager.AppSettings["SmtpHost"];
+        private static readonly int smtpPort = int.Parse(ConfigurationManager.AppSettings["SmtpPort"]);
+        private static readonly string smtpUser = ConfigurationManager.AppSettings["smtpUser"];
+        private static readonly string smtpPass = ConfigurationManager.AppSettings["smtpPass"];
+
+        private static SmtpClient smtpClient = new SmtpClient(smtpHost, smtpPort)
+        {
+            EnableSsl = true,
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential(smtpUser, smtpPass)
+        };
+
+        protected MailMessage mail = new MailMessage
+        {
+            From = new MailAddress(smtpUser, "BookWise"),
+            IsBodyHtml = true
+        };
+
+        public async void Send(string email)
+        {
+            mail.To.Add(email);
+            await smtpClient.SendMailAsync(mail);
+            mail.Dispose();
+            smtpClient.Dispose();
+        }
+    }
+}
