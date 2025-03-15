c91638724560b9415d092bdc195b1f2415859182
Chirath <chirathtimodh@gmail.com>
2025-03-15 17:31:20 +0530

    Add filter icon

--- BookWise/Properties/Resources.Designer.cs
+++ BookWise/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace BookWise.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap filter {
+            get {
+                object obj = ResourceManager.GetObject("filter", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap home {
             get {
                 object obj = ResourceManager.GetObject("home", resourceCulture);
