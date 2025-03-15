8423e76daf9dd45ab3f2214e1c9f17c5169cf0cf
Chirath <chirathtimodh@gmail.com>
2025-03-15 14:40:57 +0530

    Update Mails

--- BookWise/Mails/BookBorrowMail.cs
+++ BookWise/Mails/BookBorrowMail.cs
@@ -2,9 +2,13 @@
 {
     public class BookBorrowMail : Mail
     {
-        public BookBorrowMail()
+        public BookBorrowMail(string name, string bookTitle, int gracePeriodDays)
         {
+            string borrowDate = DateTime.Now.ToString("yyyy-MM-dd");
+            string dueDate = DateTime.Now.AddDays(gracePeriodDays).ToString("yyyy-MM-dd");
+
             mail.Subject = "You've Borrowed a Book ";
+            mail.Body = $"<div style=background:#111624;color:#fff;padding:50px;width:640px;box-sizing:border-box><div style=\"border-bottom:1px solid #232839;padding-bottom:2rem;height:30px\"><img height=30 src=https://i.ibb.co/xqqBHjrs/image.png><h2 style=display:inline;font-size:28px;margin-left:5px>BookWise</h2></div><h3 style=font-size:1.5rem;padding-top:1rem>You’ve Borrowed a Book!</h3><p style=color:#d6e0ff;font-size:18px><br>Hi {name},<br><br>You’ve successfully borrowed <span style=color:#eed1ac>{bookTitle}</span>. Here are the details:.<ul style=color:#d6e0ff;font-size:18px><li>Borrowed On: <span style=color:#eed1ac>{borrowDate}</span><li>Due Date: <span style=color:#eed1ac>{dueDate}</span></ul><p style=color:#d6e0ff;font-size:18px>Enjoy your reading, and don't forget to return the book on time!<br><br><br>Happy reading,<br>The BookWise Team</div>";
         }
     }
 }
--- BookWise/Mails/BookReturnMail.cs
+++ BookWise/Mails/BookReturnMail.cs
@@ -4,10 +4,10 @@
     {
         public BookReturnMail(string name, string bookTitle, int noOfDaysOverdue, string dueDate, float fineRate)
         {
-            string addtionalInfo = noOfDaysOverdue > 0 ? $"Next time, try to return it on time.<ul style=color:#d6e0ff;font-size:18px><li>Due Date: <span style=color:#eed1ac>{dueDate}</span><li>Overdue Since: <span style=color:#eed1ac>{noOfDaysOverdue} days</span><li>Fine Per Day: <span style=color:#eed1ac>Rs. {fineRate}</span><li>Total Fine: <span style=color:#eed1ac>Rs. {fineRate * noOfDaysOverdue}</span></ul>" : "Thank you for returning it on time.";
+            string addtionalInfo = noOfDaysOverdue > 0 ? $"Next time, try to return it on time.</p><ul style=color:#d6e0ff;font-size:18px><li>Due Date: <span style=color:#eed1ac>{dueDate}</span><li>Overdue Since: <span style=color:#eed1ac>{noOfDaysOverdue} days</span><li>Fine Per Day: <span style=color:#eed1ac>Rs. {fineRate}</span><li>Total Fine: <span style=color:#eed1ac>Rs. {fineRate * noOfDaysOverdue}</span></ul>" : "Thank you for returning it on time.</p>";
 
             mail.Subject = "Thank You for Returning the Book";
-            mail.Body = $"<div style=background:#111624;color:#fff;padding:50px;width:640px;box-sizing:border-box><div style=\"border-bottom:1px solid #232839;padding-bottom:2rem;height:30px\"><img height=30 src=https://i.imgur.com/lxRmkH4.png><h2 style=display:inline;font-size:28px;margin-left:5px>BookWise</h2></div><h3 style=font-size:1.5rem;padding-top:1rem>Thank You for Returning {bookTitle}!</h3><p style=color:#d6e0ff;font-size:18px><br>Hi {name},<br><br>We've successfully received your return of<span style=color:#eed1ac>{bookTitle}</span>. {addtionalInfo}<br><br><br>Keep the pages turning,<br>The BookWise Team</div>";
+            mail.Body = $"<div style=background:#111624;color:#fff;padding:50px;width:640px;box-sizing:border-box><div style=\"border-bottom:1px solid #232839;padding-bottom:2rem;height:30px\"><img height=30 src=https://i.ibb.co/xqqBHjrs/image.png><h2 style=display:inline;font-size:28px;margin-left:5px>BookWise</h2></div><h3 style=font-size:1.5rem;padding-top:1rem>Thank You for Returning {bookTitle}!</h3><p style=color:#d6e0ff;font-size:18px><br>Hi {name},<br><br>We've successfully received your return of<span style=color:#eed1ac>{bookTitle}</span>. {addtionalInfo}<br><br><p style=color:#d6e0ff;font-size:18px>Keep the pages turning,<br>The BookWise Team</p></div>";
         }
     }
 }
