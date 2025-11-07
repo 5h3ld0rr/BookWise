74e88c3a0a592b8f46b52707deb387c89fb89266
Chirath <chirathtimodh@gmail.com>
2025-11-08 03:27:50 +0530

    update sql

--- BookWise.sql
+++ BookWise.sql
@@ -1,25 +1,7 @@
--- --------------------------------------------------------
--- Host:                         vps.technovizion.studio
--- Server version:               11.4.5-MariaDB-0ubuntu0.24.10.1 - Ubuntu 24.10
--- Server OS:                    debian-linux-gnu
--- HeidiSQL Version:             12.10.0.7000
--- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Dumping database structure for BookWise
-CREATE DATABASE IF NOT EXISTS `BookWise` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
+CREATE DATABASE IF NOT EXISTS `BookWise`;
 USE `BookWise`;
 
--- Dumping structure for table BookWise.books
 CREATE TABLE IF NOT EXISTS `books` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
@@ -28,11 +10,9 @@ CREATE TABLE IF NOT EXISTS `books` (
   `category` varchar(255) DEFAULT NULL,
   `available_books` int(11) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+);
 
--- Data exporting was unselected.
 
--- Dumping structure for table BookWise.book_transactions
 CREATE TABLE IF NOT EXISTS `book_transactions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `book_id` int(11) NOT NULL,
@@ -44,20 +24,17 @@ CREATE TABLE IF NOT EXISTS `book_transactions` (
   KEY `FK__Users` (`user_id`),
   CONSTRAINT `FK__Books` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   CONSTRAINT `FK__Users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ;
 
--- Data exporting was unselected.
-
--- Dumping structure for table BookWise.rules
 CREATE TABLE IF NOT EXISTS `rules` (
   `max_books_per_user` int(11) NOT NULL DEFAULT 2,
   `max_days_to_return` int(11) NOT NULL DEFAULT 14,
   `fine_per_day` decimal(20,2) NOT NULL DEFAULT 5.00
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ;
 
--- Data exporting was unselected.
+INSERT INTO `rules` (`max_books_per_user`, `max_days_to_return`, `fine_per_day`) VALUES
+	(2, 14, 5.00);
 
--- Dumping structure for table BookWise.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
   `first_name` varchar(255) NOT NULL,
@@ -72,12 +49,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `nic` (`nic`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+);
 
--- Data exporting was unselected.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `role`, `nic`, `phone`, `address`, `email`, `password`, `created_at`) VALUES
+	(123456, 'Admin', 'Admin', 'admin', '123456789', NULL, NULL, 'admin@gmaill.com', '$2a$11$ntUgp6eHfEuy1Tdz.otyi.jlzxLfHgEqEQJWg5Mje2Ywc2QRST9ES', '2025-11-07 16:23:28');
